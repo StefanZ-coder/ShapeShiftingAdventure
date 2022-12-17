ed5ffde1e0638dac8f61357c2aaf89d9ed5b8a37
stefa <stefa@DESKTOP-5Q9K2GO>
2022-12-17 15:11:40 +0100

    Projektdateien hinzufügen.

---
+++ Spiele Versuch/Playerstuff.cs
@@ -0,0 +1,52 @@
+﻿
+
+public class Playerstuff
+{
+
+    public static Random rand = new Random();
+    public Playerstuff(int health, int mana, int potions, int dps, int armor,int weapon)
+    {
+
+        Health = health;  
+        Mana = mana;
+        Potions = potions;
+        Dps = dps;
+        Armor = armor;
+        Weapon = weapon;
+        
+    }
+
+public string Name { get; set; }
+public int Health { get; set; }
+public int Mana { get; set; }
+public int Gold { get; set; }
+public int Potions { get; set; }
+public int Dps { get; set; }
+public int Armor { get; set; }
+public int Weapon { get; set; }
+
+
+    public int modifyer = 0;
+
+    public int GetPower()
+    {
+        int upper = (2 * modifyer + 3);
+        int lower = (modifyer + 2);
+        return rand.Next(lower, upper);
+    }
+
+    public int GetHealth()
+    {
+        int upper = (2 * modifyer + 6);
+        int lower = (modifyer + 2);
+        return rand.Next(lower, upper);
+    }
+
+    public int GetGold()
+    {
+        int upper = (2 * modifyer + 8);
+        int lower = (modifyer + 2);
+        return rand.Next(lower, upper);
+    }
+
+}
