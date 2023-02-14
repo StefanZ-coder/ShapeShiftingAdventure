0a514d79dbe9f8bae04f07314fa914b5af9499de
Stefan <stefanzimny25@gmail.com>
2023-02-14 21:25:50 +0100

    MeeleWeapon wurde gelöscht^^ nun ist sie wieder da :P

---
+++ SpieleVersuchCommon1/MeeleWeapon.cs
@@ -0,0 +1,12 @@
+﻿public class MeeleWeapon : Item
+{
+
+    public MeeleWeapon(int damage)
+    {
+        Damage = damage;
+    }
+
+    public int Damage { get; }
+    protected override string Name => "stumpfes Schwert";
+
+}
